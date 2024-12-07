91a089a5b3c705245afd65fdfb610547d8ce3a06
max <max.assis@chatguru.com.br>
2024-12-07 20:27:04 -0300

    feat: fazendo a pagina de listar tasks

--- app/(app)/(tasks)/taskList.tsx
+++ app/(app)/(tasks)/taskList.tsx
@@ -1,15 +1,212 @@
+// import { useLocalSearchParams } from 'expo-router';
+// import { Text, View } from 'react-native';
+
+// export default function TaskList() {
+//   const { participationId, desafioName } = useLocalSearchParams();
+
+//   console.log(participationId, desafioName); // Verifique os valores recebidos no console.
+
+//   return (
+//     <View>
+//       <Text>Tarefa relacionada ao desafio: {desafioName ?? "hehehe"}</Text>
+//       <Text>ID de participação: {participationId ?? "dsadasdd"}</Text>
+//     </View>
+//   );
+// }
+
+
+import { useEffect, useState, useRef, useMemo, useCallback } from "react";
 import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import tokenExists from "../../../store/auth-store";
+import { SafeAreaView, View, Text, ScrollView, TouchableOpacity, Alert } from "react-native";
+import Left from "../../../assets/Icon-left.svg";
+import TaskItem from "../../../components/taskItem";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import Plus from "../../../assets/plus.svg";
 
-export default function TaskList() {
+export type TasksData = Data[];
+export interface Data {
+  id: number;
+  name: string;
+  environment: string;
+  date: null | Date;
+  duration: null | string;
+  calories: number;
+  local: null | string;
+  distanceKm: string;
+  participationId: number;
+  usersId: string;
+}
+
+export default function TaskList({ route }: any) {
   const { participationId, desafioName } = useLocalSearchParams();
+  const navigation = useNavigation<any>();
+  const token = tokenExists((state) => state.token);
+  const [data, setData] = useState<TasksData>();
+  const [task, setTask] = useState<Data>();
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const bottomSheetEditRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ["30%"], []);
+  const snapPointsEdit = useMemo(() => ["20%"], []);
 
-  console.log(participationId, desafioName); // Verifique os valores recebidos no console.
+  const fetchTasks = useCallback(() => {
+    fetch(`http://192.168.1.18:3000/tasks/get-tasks/${participationId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json() as Promise<TasksData>)
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => console.error(error));
+  }, [participationId, token]);
+
+  function deleteTask(id: number) {
+    Alert.alert(
+      "Confirmação de Exclusão", 
+      "Tem certeza que deseja excluir esta tarefa?",
+      [
+        {
+          text: "Cancelar",
+          style: "cancel", 
+        },
+        {
+          text: "Excluir",
+          style: "destructive", 
+          onPress: () => {
+            fetch(`http://192.168.1.18:3000/tasks/delete-task/${id}`, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            })
+              .then((response) => {
+                if (!response.ok) {
+                  throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                bottomSheetEditRef.current?.close()
+                return response.json();
+              })
+              .then((json) => {
+                console.log("Task deleted:", json);
+                fetchTasks(); 
+              })
+              .catch((error) => {
+                console.error("Failed to delete the task:", error);
+                Alert.alert("Erro ao excluir tarefa", "", [
+                  {
+                    text: "Ok",
+                    style: "cancel",
+                  }
+                ])
+              });
+          },
+        },
+      ],
+      { cancelable: true } // O alerta pode ser cancelado ao tocar fora dele
+    );
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchTasks();
+    }, [fetchTasks])
+  );
+
+  function openModalEdit(taskData: Data) {
+    setTask(taskData);
+    bottomSheetEditRef.current?.expand()
+  }
 
   return (
-    <View>
-      <Text>Tarefa relacionada ao desafio: {desafioName ?? "hehehe"}</Text>
-      <Text>ID de participação: {participationId ?? "dsadasdd"}</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <ScrollView overScrollMode="never" className="bg-[#F1F1F1] flex-1 ">
+        <View className="bg-white mb-7">
+          <View className="flex-row mt-[49.5] px-5 bg-white">
+            <TouchableOpacity className="w-[30px] h-[30px]" onPress={() => navigation.navigate("DesafioSelect")} >
+              <Left />
+            </TouchableOpacity>
+            <Text className="text-base font-inter-bold mx-auto ">
+              Atividades recentes
+            </Text>
+          </View>
+
+          <View className="h-[60px] mt-4 pt-2 px-5 mb-7 bg-white">
+            <Text className="text-sm text-bondis-gray-secondary">Desafio</Text>
+            <Text className="text-base font-inter-bold mt-2">
+              {desafioName}
+            </Text>
+          </View>
+        </View>
+
+        {data && data.map((task) => (
+             <TaskItem task={task} key={task.id} openModalEdit={openModalEdit} />
+        ))}
+
+      </ScrollView>
+
+      <TouchableOpacity
+          onPress={() => bottomSheetRef.current?.expand()}
+          className="rounded-full bg-bondis-green absolute w-16 h-16 justify-center items-center right-4 bottom-6"
+        >
+          <Plus />
+        </TouchableOpacity>
+
+        <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={snapPoints}
+        index={-1}
+        enablePanDownToClose
+        backgroundStyle={{
+          borderRadius: 20,
+        }}
+      >
+        <BottomSheetView className="flex-1">
+          <Text className="font-inter-bold mt-[10px] text-base mx-5 mb-4">
+            Adicione um atividade
+          </Text>
+          <View className="mx-5">
+            <View className="h-[51px] justify-center items-center border-b-[0.2px] border-b-gray-400">
+              <Text>Via Strava</Text>
+            </View>
+            <View className="h-[51px] justify-center items-center border-b-[0.2px] border-b-gray-400">
+              <Text>Via Apple Saúde</Text>
+            </View>
+            <TouchableOpacity onPress={() => navigation.navigate("TaskCreate", { participationId, desafioName })}  className="h-[51px] justify-center items-center border-b-[0.2px] border-b-gray-400">
+              <Text>Cadastrar manualmente</Text>
+            </TouchableOpacity>
+          </View>
+        </BottomSheetView>
+      </BottomSheet>
+
+      <BottomSheet
+        ref={bottomSheetEditRef}
+        snapPoints={snapPointsEdit}
+        index={-1}
+        enablePanDownToClose
+        backgroundStyle={{
+          borderRadius: 20,
+        }}
+      >
+        <BottomSheetView className="flex-1">
+          <View className="mx-5">
+            <TouchableOpacity onPress={() => { navigation.navigate("TaskEdit", { participationId: participationId, taskData: task, desafioName }); bottomSheetEditRef.current?.close()}} 
+            className="h-[51px] justify-center items-center border-b-[0.2px] border-b-gray-400">
+              <Text>Editar atividade</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => deleteTask(task!.id)} className="h-[51px] justify-center items-center">
+              <Text className="text-bondis-alert-red">Excluir atividade</Text>
+            </TouchableOpacity>
+          </View>
+        </BottomSheetView>
+      </BottomSheet>  
+
+    </SafeAreaView>
   );
 }
---
+++ components/taskItem.tsx
@@ -0,0 +1,113 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import Livre from "../assets/livre.svg";
+import Calendar from "../assets/calendar.svg";
+import Pin from "../assets/map-pin.svg";
+import Gear from "../assets/settings-black.svg";
+import Link from "../assets/link.svg";
+import { useNavigation } from "@react-navigation/native";
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
+
+export interface TaskItemProps {
+  id: number
+  name: string
+  environment: string
+  date: Date | null
+  duration: string | null
+  calories: number
+  local: string | null
+  distanceKm: string
+  participationId: number
+  usersId: string
+}
+
+export interface TaskListProps {
+  task: TaskItemProps
+  openModalEdit: (taskData: TaskItemProps) => void
+}
+
+function convertISOToTime(isoString: string): string {
+  const date = new Date(isoString);  
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+const timeSinceDate = (dateStr: string | Date): string => {
+  const targetDate = dayjs(dateStr);
+  if (!targetDate.isValid()) {
+    throw new Error("Data inválida");
+  }
+  const currentDate = dayjs();
+  const differenceInYears = currentDate.diff(targetDate, 'year');
+  const differenceInMonths = currentDate.diff(targetDate, 'month');
+  const differenceInDays = currentDate.diff(targetDate, 'day');
+  if (differenceInYears >= 1) {
+    return `Há ${differenceInYears} ano(s)`;
+  } else if (differenceInMonths >= 1) {
+    return differenceInMonths === 1 ? `Há 1 mês` : `Há ${differenceInMonths} meses`;
+  } else {
+    return differenceInDays === 1 ? `Há 1 dia` : `Há ${differenceInDays} dias`;
+  }
+};
+
+export default function TaskItem({ task, openModalEdit }: TaskListProps) {
+    const navigation = useNavigation<any>();
+
+    return(
+        <View className="h-[165px] p-5 bg-white mb-4">
+          <View className="flex-row w-full h-[42px]">
+            <View className="h-[42px] flex-row">
+              <Livre />
+              <View className="ml-4 ">
+                <Text className="text-base font-inter-bold">
+                  {task.name}
+                </Text>
+                <View className="flex-row">
+                  <View className="flex-row gap-x-1 items-center justify-center">
+                    <Calendar />
+                    <Text className="text-bondis-gray-dark text-xs">
+                    {timeSinceDate(task.date!)}
+                    </Text>
+                  </View>
+                  <View className="flex-row gap-x-1 items-center justify-center ml-4">
+                    {task.local && <Pin />}
+                    <Text className="text-bondis-gray-dark text-xs ml-4">
+                      {task.local}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+
+            <TouchableOpacity onPress={() => openModalEdit(task)} className="ml-auto w-[40px] h-[32px] items-end">
+              <Gear />
+            </TouchableOpacity>
+          </View>
+
+          <View className="flex-row items-center gap-x-1 mt-3 none">
+            <Link />
+            <Text className="text-xs text-bondis-gray-dark">Cadastrado manualmente</Text>
+          </View> 
+
+          <View className="flex-row mt-3">
+            <View className="w-[98px] h-[44px] border-l-2 border-[#D1D5DA] pl-2">
+                <Text className="text-[18px] font-inter-bold">{task.distanceKm}</Text>
+                <Text className="text-bondis-gray-dark text-[10px]">KM</Text>
+            </View>
+            <View className="w-[100px] h-[44px] border-l-2 border-[#D1D5DA] pl-2">
+                <Text className="text-[18px] font-inter-bold">{ convertISOToTime(task.duration!) }</Text>
+                <Text className="text-bondis-gray-dark text-[10px]">DURAÇÃO</Text>
+            </View>
+            <View className="w-[98px] h-[44px] border-l-2 border-[#D1D5DA] pl-2">
+                <Text className="text-[18px] font-inter-bold">{task.calories}</Text>
+                <Text className="text-bondis-gray-dark text-[10px]">CAL</Text>
+            </View>
+          </View> 
+        </View>
+    )
+}
