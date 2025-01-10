1eef96b4778cbd7c6930e7782ba4f39c57c435d9
max <max.assis@chatguru.com.br>
2025-01-10 18:52:09 -0300

    fazendo pagina de compra

--- app/(app)/buy.tsx
+++ app/(app)/buy.tsx
@@ -74,11 +74,59 @@ export default function Buy() {
           </View>
         </View>
 
-        <TouchableOpacity className="h-[52px] bg-bondis-green mt-[45px] mb-4 rounded-full justify-center mx-5">
+        {/* <TouchableOpacity className="h-[52px] bg-bondis-green mt-[45px] mb-4 rounded-full justify-center mx-5">
           <Text className="text-center font-inter-bold text-base">
             Quero escolher meu kit
           </Text>
+        </TouchableOpacity> */}
+
+
+          <Text className="mx-5 mt-8 text-base font-inter-bold">Descrição:</Text>
+
+          <Text className="mx-5 mt-4 text-base text-justify">Bem-vindo ao Desafio Cidade Maravilhosa, uma jornada única que leva você a percorrer virtualmente 150 km pelos pontos mais icônicos e deslumbrantes do Rio de Janeiro! Este desafio é uma oportunidade imperdível para corredores de todos os níveis, proporcionando uma experiência enriquecedora e motivadora enquanto você se mantém ativo e saudável</Text>
+
+          <View className="mx-5 mt-8 p-4 border-[1px] border-[#D9D9D9] rounded-md">
+            <Text className="text-base font-inter-bold mb-[10px]">Percurso</Text>
+            <Image className="w-full" source={require("../../assets/map.png")} />
+          </View>
+
+          <View className="mx-5 mt-8 p-4 border-[1px] border-[#D9D9D9] rounded-md">
+            <Text className="text-base font-inter-bold mb-[10px]">Como participar?</Text>
+            <Text className="text-base text-bondis-gray-dark text-justify">Para participar, inscreva-se agora e pague a taxa de inscrição. Após a confirmação do pagamento, você estará oficialmente inscrito e pronto para iniciar sua jornada. Lembre-se de confirmar seu endereço para receber os itens de recompensa ao final do desafio.</Text>
+
+            <Text className="text-base text-bondis-gray-dark mt-8">Preço: R$ 121,00</Text>
+          </View>
+
+          <View className="mx-5 mt-8 p-4 border-[1px] border-[#D9D9D9] rounded-md">
+            <Text className="text-base font-inter-bold mb-[10px]">Benefícios</Text>
+            
+            <Text className="text-base text-bondis-gray-dark text-justify">Camisa: Uma camisa exclusiva do Desafio Cidade Maravilhosa 150km.</Text>
+
+            <Text className="text-base text-bondis-gray-dark mt-8 text-justify">Garrafa personalizada: Uma garrafa d'agua personalizada do desafio.</Text>
+
+            <Text className="text-base text-bondis-gray-dark mt-8 text-justify">Medalha de conclusão: Apos completar 100% do desafio, solicite e receba em casa sua medalha de finisher.</Text>
+
+            <Text className="text-base text-bondis-gray-dark mt-8 text-justify">Ranking e Perfil Social: A todo momento acompanhe seu progresso e compare seu desempenho com outros participantes.</Text>
+          </View>
+
+          <View className="mx-5 mt-8 p-4 border-[1px] border-[#D9D9D9] rounded-md">
+            <Text className="text-base font-inter-bold mb-[10px]">Regras</Text>
+            
+            <Text className="text-base text-justify text-bondis-gray-dark">1. Inscreva-se no desafio pelo valor de R$ 120,00 e agyarde a chegada do seu Kit starter no endereço informado</Text>
+
+            <Text className="text-base text-justify text-bondis-gray-dark mt-8">2. Rastreie sua atividade de corrida ou caminhada através do dispositivos e aplicativos compativeis, como smartwatches e Strava.</Text>
+
+            <Text className="text-base text-justify text-bondis-gray-dark mt-8">3. Cada quilometro importa! As distâncias acumuladas serão automaticamente registradas no mapa do desafio. Mantenha sua rotina de exercicios até concluir todo o desafio. </Text>
+
+            <Text className="text-base text-justify text-bondis-gray-dark mt-8">4. Ao final do desafio, você poderá reinvindicar o seu kit finisher e compartilhar com o mundo a sua conquista.</Text>
+          </View>
+
+          <TouchableOpacity className="h-[52px] bg-bondis-green mt-[45px] mb-4 rounded-full justify-center mx-5">
+          <Text className="text-center font-inter-bold text-base">            
+            Aceito o desafio 💪
+          </Text>
         </TouchableOpacity>
+
       </ScrollView>
     </SafeAreaView>
   );
