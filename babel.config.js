ee15969b6f11321da6ed8499977b8fc29280f5a0
max <max.assis@chatguru.com.br>
2025-05-04 11:21:04 -0300

    feat: migrando para o expo 53

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,43 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo',{
+//       // Habilita a transformação de `import.meta`
+//       unstable_transformImportMeta: true,
+//     }],
+//     plugins: [
+//       "nativewind/babel",
+//       ["module:react-native-dotenv", {
+//         "moduleName": "@env",
+//         "path": ".env",
+//         "blacklist": null,
+//         "whitelist": null,
+//         "safe": false,
+//         "allowUndefined": true
+//       }]
+//     ]
+//   };
+// };
+
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    // ✅ Preset com opções precisa estar em um array próprio
+    presets: [
+      ['babel-preset-expo', {
+        unstable_transformImportMeta: true,
+      }],
+    ],
     plugins: [
-      "nativewind/babel",
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-      }]
-    ]
+      'nativewind/babel',
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      }],
+    ],
   };
 };
